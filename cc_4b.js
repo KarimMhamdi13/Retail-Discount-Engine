870c28ceecb32a84ddf719f2adaabb664b5dbff8
Karim Mhamdi <karimmhamdi@usf.edu>
2025-09-17 20:27:34 -0400

    Step 3 done

--- cc_4b.js
+++ cc_4b.js
@@ -3,20 +3,30 @@
 const products = [
     { sku: "123", name: "Toothbrush", category: "Hygiene", price: 3.99, inventory: 125 },
     { sku: "234", name: "T-shirt", category: "Apparel", price: 7.99, inventory: 45 },
-    { sku: "345", name: "Laptop", category: "Electronics", price: 599.99, inventory: 200 },
+    { sku: "345", name: "Laptop", category: "Electronics", price: 599.99, inventory: 20 },
     { sku: "456", name: "Sponge", category: "Household", price: 3.50, inventory: 75 },
     { sku: "567", name: "Milk", category: "Groceries", price: 5.99, inventory: 60 },
 ];
 
 for (let product of products) {
-    let discount = 0;
+    let discount;
     switch (product.category) {
         case "Electronics":
             discount = 0.2
+            break;
+        case "Apparel":
+            discount = 0.15
             break;
         case "Groceries":
         case "Household":
             discount = 0.1
             break;
+        default:
+            discount = 0;
+            break;
         
+    }
+    let promoPrice = product.price * [1-discount];
+    product.promoPrice = promoPrice;
+
 }
